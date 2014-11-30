bd598bbce4c860c03a7d152d0caca47f76171a98
Richard Larocque <richard.larocque@gmail.com>
2014-11-29 19:27:04 -0800

    Found problems

--- Tests/HUnit/Interop.hs
+++ Tests/HUnit/Interop.hs
@@ -1,6 +1,7 @@
 module Tests.HUnit.Interop(tests) where
 
 import           Control.Monad
+import qualified Data.ByteString            as B
 import           Data.Either
 import           Data.Serialize
 import           Ethereum.State.Block
@@ -10,22 +11,25 @@ import           Tests.TestData             as D
 
 import           Ethereum.BlockVerification
 
-numberedBlocks :: [(Integer, Either String Block)]
-numberedBlocks = zip [1..] $ map (runGet getBlock) D.blocks
+singleBlockTest :: String -> ((B.ByteString, Either String Block) -> Assertion) -> TestTree
+singleBlockTest name test = testGroup name $ map f (zip [1..] D.blocks)
+    where f (n, b) = testCase ("Block " ++ show n) (test (b, runGet getBlock b))
 
-singleBlockTest :: String -> (Either String Block -> Assertion) -> TestTree
-singleBlockTest name test = testGroup name $ map f numberedBlocks
-    where f (n, b) = testCase ("Block " ++ show n) (test b)
+checkParse :: (B.ByteString, Either String Block) -> Assertion
+checkParse (_, b) = case b of
+                      Left err -> assertFailure err
+                      Right _ -> return ()
 
-checkParse :: Either String Block -> Assertion
-checkParse b = case b of
-                 Left err -> assertFailure err
-                 Right _ -> return ()
+checkReserialize :: (B.ByteString, Either String Block) -> Assertion
+checkReserialize (orig, p) =
+    case p of
+      Left _   -> assertFailure "bad parse"
+      Right p' -> orig @=? runPut (putBlock p')
 
 singleBlockTest' :: String -> (Block -> Assertion) -> TestTree
 singleBlockTest' name test = singleBlockTest name test'
-    where test' (Left _ ) = assertFailure "bad parse"
-          test' (Right b) = test b
+    where test' (_, Left _ ) = assertFailure "bad parse"
+          test' (_, Right b) = test b
 
 checkNonce :: Block -> Assertion
 checkNonce b = unless (isBlockNonceValid $ header b) (assertFailure "bad nonce")
@@ -33,5 +37,6 @@ checkNonce b = unless (isBlockNonceValid $ header b) (assertFailure "bad nonce")
 tests :: TestTree
 tests = testGroup "Interop" [
          singleBlockTest "Parse" checkParse,
+         singleBlockTest "Re-Serialize" checkReserialize,
          singleBlockTest' "Nonce" checkNonce
         ]
