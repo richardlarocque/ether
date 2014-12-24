9df893f98dccd1ee4910f34a69c1a6d1ce61c299
Richard Larocque <richard.larocque@gmail.com>
2014-12-24 21:05:49 +0100

    Working with Nix (kinda)

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
+let
+  inherit (haskellPackages) cabal cabalInstall_1_18_0_3
+    largeword vector cryptohash
+    cryptoRandom cryptoPubkey byteable
+    cereal mtl dataLens;
+
+    # array containers bytestring OddWord
+
+in cabal.mkDerivation (self: {
+  pname = "ether";
+  version = "0.1.0";
+  src = ./.;
+  buildDepends = [
+    # As imported above
+    largeword vector cryptohash
+    cryptoRandom cryptoPubkey byteable
+    cereal mtl dataLens
+  ];
+  buildTools = [ cabalInstall_1_18_0_3 ];
+  enableSplitObjs = false;
+})
